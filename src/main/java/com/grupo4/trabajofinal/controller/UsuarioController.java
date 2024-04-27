8566e3e3e20f43d9e7cf3ab854874cb83c7dcfdf
AleJM11 <118072786+AleJM11@users.noreply.github.com>
2024-04-27 17:28:05 -0500

    Update UsuarioController.java

--- src/main/java/com/grupo4/trabajofinal/controller/UsuarioController.java
+++ src/main/java/com/grupo4/trabajofinal/controller/UsuarioController.java
@@ -1,23 +1,38 @@
 package com.grupo4.trabajofinal.controller;
 
-import com.grupo4.trabajofinal.dto.UsuarioLoginDto;
-import com.grupo4.trabajofinal.model.Usuario;
-import com.grupo4.trabajofinal.service.UsuarioLoginService;
+import com.upc.tp_alejandroversion.dto.PaisDto;
+import com.upc.tp_alejandroversion.dto.UsuarioDto;
+import com.upc.tp_alejandroversion.model.Pais;
+import com.upc.tp_alejandroversion.model.Usuario;
+import com.upc.tp_alejandroversion.service.UsuarioService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
-    final UsuarioLoginService usuarioLoginService;
+    final UsuarioService usuarioService;
 
-    public UsuarioController(UsuarioLoginService usuarioLoginService) {
-        this.usuarioLoginService = usuarioLoginService;
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
     }
-    @PostMapping("/login")
-    public ResponseEntity<Usuario> login(@RequestBody UsuarioLoginDto usuarioLoginDto) {
-        Usuario usuarioAutenticado = usuarioLoginService.login(usuarioLoginDto);
-        return ResponseEntity.ok(usuarioAutenticado);
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDto>> getAll() {
+        return new ResponseEntity<>(usuarioService.getAll(), HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioDto> GetById(@PathVariable ("id") Integer id) {
+        UsuarioDto usuarioDto = usuarioService.GetById(id);
+        if (usuarioDto == null)
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(usuarioDto, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<Usuario> Add(@RequestBody UsuarioDto usuarioDto){
+        return new ResponseEntity<>(usuarioService.Add(usuarioDto), HttpStatus.CREATED);
     }
 }
