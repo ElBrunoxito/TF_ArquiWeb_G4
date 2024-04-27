6d9ab24687be85072c64b93aea051956bd97fb7b
AleJM11 <118072786+AleJM11@users.noreply.github.com>
2024-04-27 17:24:46 -0500

    Update Usuario.java

--- src/main/java/com/grupo4/trabajofinal/model/Usuario.java
+++ src/main/java/com/grupo4/trabajofinal/model/Usuario.java
@@ -6,32 +6,32 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Usuario {
+public class Usuario{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String nombre;
-    private String apellido;
-    private String usuario;
-    private String email;
+    private String nombreUsuario;
+    private String apellidoUsuario;
+    private String emailUsuario;
     private String contrasena;
     private Date fechaRegistro = new Date();
     private String direccionUsuario;
     private String telefonoUsuario;
-    //relacion 1 a muchos de ciudad a usuario
-    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
-    @JoinColumn(name = "ciudad_id")
-    private Ciudad ciudad;
-    //relacion de 1 a 1 de usuario y TD
-    @OneToOne(mappedBy = "usuario" ,cascade = CascadeType.ALL)
-    private TarjetaDebito tarjetaDebito;
 
-    //Constructor sin Id, sin lista de tarjetas, sin ciudad
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.ALL)
+    List<TarjetaDebito> tarjetas;
+
+    //Referencias
+    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @JoinColumn(name = "idCiudad")
+    private Ciudad ciudad;
+
     public Usuario(String nombreUsuario, String apellidoUsuario, String emailUsuario, String contrasena, Date fechaRegistro, String direccionUsuario, String telefonoUsuario) {
         this.nombreUsuario = nombreUsuario;
         this.apellidoUsuario = apellidoUsuario;
