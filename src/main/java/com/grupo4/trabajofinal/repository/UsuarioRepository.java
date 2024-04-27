fd2faf87622d4c9e1b6d578435484c171356aac5
AleJM11 <118072786+AleJM11@users.noreply.github.com>
2024-04-27 15:43:49 -0500

    Update UsuarioRepository.java

--- src/main/java/com/grupo4/trabajofinal/repository/UsuarioRepository.java
+++ src/main/java/com/grupo4/trabajofinal/repository/UsuarioRepository.java
@@ -1,1 +1,9 @@
+package com.upc.tp_alejandroversion.repository;
 
+import com.upc.tp_alejandroversion.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+}
